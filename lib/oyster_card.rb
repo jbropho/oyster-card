0168e5d60ce2f1ca376fe7c66d7d1c44b2be9805
Andrew Kemp <akemp850@gmail.com>
2018-03-26 17:16:36 +0100

    Added deduct feature and test

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,16 +1,18 @@
-# frozen_string_literal: true
-
 class OysterCard
   attr_reader :balance
   MAX_CAPACITY = 90
 
-  def initialize
-    @balance = 0
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def top_up(amount)
     raise "Max balance of #{MAX_CAPACITY} exceeded" if exceed_limit?(amount)
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 
   private
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,13 +1,7 @@
-# frozen_string_literal: true
-
 require 'oyster_card'
 
 describe OysterCard do
   subject(:oyster_card) { OysterCard.new }
-
-  it 'responds to balance' do
-    expect(oyster_card).to respond_to(:balance)
-  end
 
   it 'responds with a balance of 0' do
     expect(oyster_card.balance).to eq 0
@@ -23,6 +17,14 @@ describe OysterCard do
   describe '.exceed_limit?' do
     it 'returns true when limit exceeded' do
       expect(oyster_card.send(:exceed_limit?, 91)).to eq(true)
+    end
+  end
+
+  describe '.deduct' do
+    subject(:oyster_card) { OysterCard.new(10) }
+    it 'deducts specified amount' do
+      oyster_card.deduct(5)
+      expect(oyster_card.balance).to eq 5
     end
   end
 end
