6de0f05e08c40a59743dc76647942406b7708d93
Jordan Brooks <jordanbrooks@MacBook-Pro-9.local>
2018-03-26 14:24:30 +0100

    passing test initialize oyster card with default balance

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,4 +1,13 @@
 require 'oyster_card'
 
-describe OysterCard do 
+describe OysterCard do
+  subject(:oyster_card) { OysterCard.new }
+
+  it 'responds to balance' do 
+    expect(oyster_card).to respond_to(:balance)
+  end 
+
+  it 'responds with a balance of 0' do 
+    expect(oyster_card.balance).to eq 0
+  end 
 end 
