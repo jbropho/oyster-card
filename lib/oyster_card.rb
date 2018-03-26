761174348c8bdfa82cc2b4a2928c6e0c80596bbf
Andrew Kemp <akemp850@gmail.com>
2018-03-26 15:52:19 +0100

    Added top-up method and test

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,6 +1,16 @@
 class OysterCard
   attr_reader :balance
-  def initialize 
-    @balance = 0 
+  MAX_CAPACITY = 90
+
+  def initialize
+    @balance = 0
   end
-end 
+
+  def top_up(amount)
+    raise StandardError unless @balance + amount <= 90
+    @balance += amount
+  end
+
+
+
+end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -3,11 +3,34 @@ require 'oyster_card'
 describe OysterCard do
   subject(:oyster_card) { OysterCard.new }
 
-  it 'responds to balance' do 
+  it 'responds to balance' do
     expect(oyster_card).to respond_to(:balance)
-  end 
+  end
 
-  it 'responds with a balance of 0' do 
+  it 'responds with a balance of 0' do
     expect(oyster_card.balance).to eq 0
-  end 
-end 
+  end
+
+  describe '.top_up' do
+    it 'increases balance by specified amount' do
+      oyster_card.top_up(10)
+      expect(oyster_card.balance).to eq 10
+    end
+
+    context 'when attempting to exceed top_up limit' do
+      it 'raises an error' do
+        oyster_card.top_up(OysterCard::MAX_CAPACITY)
+        expect{ oyster_card.top_up(1) }.to raise_error StandardError
+      end
+    end
+  end
+
+  # describe '.exceed_limit?' do
+  #   allow(oyster_card).to receive(:balance).and_return 90
+  #   expect
+  #
+  # end
+
+
+
+end
